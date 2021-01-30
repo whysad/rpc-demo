bbcfb44cc293f79f3b04c22eb11c57c0ea7ac907
tanzhenhong <tanzhenhong@maoyan.com>
2021-01-30 10:45:42 +0800

    update: provider and consumer init

---
+++ provider/src/main/java/com/example/rpc/provider/processor/ZkBeanPostProcessor.java
@@ -0,0 +1,15 @@
+package com.example.rpc.provider.processor;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+
+public class ZkBeanPostProcessor implements BeanFactoryPostProcessor {
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory configurableListableBeanFactory) throws BeansException {
+        String[] beanDefinitionNames = configurableListableBeanFactory.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+
+        }
+    }
+}
